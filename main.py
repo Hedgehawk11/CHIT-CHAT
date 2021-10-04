c15b68867dd7b8d67eca139476670ff68e7e5d34
dbates <84346970+dbates314@users.noreply.github.com>
2021-10-04 17:14:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from replit import db
+
+name = ('what do you want to be called')
+
+x = 1
+while x == 1:
+	say = input('what do you want to say(if you want to see the last chit say see)')
+	if say == 'see'
