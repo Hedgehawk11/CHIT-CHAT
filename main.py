7ba0e2a560fd8f61fc8ec87a1e5c250aae682399
dbates314 <84346970+dbates314@users.noreply.github.com>
2021-10-05 21:12:54 +0000

    it works

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from replit import db
 
-name = ('what do you want to be called')
+name = input('what do you want to be called ')
+
 
 x = 1
 while x == 1:
-	say = input('what do you want to say(if you want to see the last chit say see)')
-	if say == 'see'
+	say = input('what do you want to say(if you want to see the last chit say see) ')
+	if say == 'see':
+		value = db["message"]
+		print(value)
+	else:
+		db["message"] = name + ' said:' + say
